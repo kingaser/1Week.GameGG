b276f7f48ded8ab151965044b47f4e6ab6812723
JellyKingdom <jyhan1993@gmail.com>
2023-01-17 02:26:39 +0900

    feat: fix app.py

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.athumb_user.find_one({"id": payload['id']})
-        return render_template('main.html', nickname=user_info["nickname"])
+        user_info = db.user.find_one({"id": payload['id']})
+        return render_template('main.html', nickname=user_info["nick"])
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인이 필요합니다."))
     except jwt.exceptions.DecodeError:
@@ -183,5 +183,5 @@ def review_get():
 
 #여기까지 입니다
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
 
